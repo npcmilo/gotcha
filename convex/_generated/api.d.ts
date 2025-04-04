a9fb5a245dd1d76d55cd31aec8f0de8824ff0c3d
Kenny Oseleononmen <oseleonose@gmail.com>
2025-04-03 20:41:47 -0700

    🔥 function for doing the migration

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,9 @@ import type {
 } from "convex/server";
 import type * as images from "../images.js";
 import type * as levels from "../levels.js";
+import type * as migration from "../migration.js";
+import type * as models_airtable from "../models/airtable.js";
+import type * as models_migration from "../models/migration.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -27,6 +30,9 @@ import type * as levels from "../levels.js";
 declare const fullApi: ApiFromModules<{
   images: typeof images;
   levels: typeof levels;
+  migration: typeof migration;
+  "models/airtable": typeof models_airtable;
+  "models/migration": typeof models_migration;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
